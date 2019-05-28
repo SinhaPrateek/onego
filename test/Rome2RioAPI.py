6fb18c85bdf20cc5ae71354a09aaa323bfd810ba
ARUNESH NANDAN <nandanarunesh@gmail.com>
2019-05-28 15:48:30 +0530

    Tested the Rome2Rio API with lat long and printed the response data in a familiar way

--- test/Rome2RioAPI.py
+++ test/Rome2RioAPI.py
@@ -9,14 +9,17 @@ api_url_base = 'http://free.rome2rio.com/api/1.4/json/Search'
 
 origin_palce = 'delhi'
 dest_place = 'patna'
+origin_position = '28.646811,77.236252' # Jama Masjid, New Delhi
+dest_position = '12.891430,77.606113' # Ayyappa Swami Temple, Vijaya Bank Layout
 
-url_arguments = {'key':api_token,'oName':origin_palce,'dName':dest_place}
+url_arguments_with_place_name = {'key':api_token,'oName':origin_palce,'dName':dest_place}
+url_arguments_with_place_latlong = {'key':api_token,'oPos':origin_position,'dPos':dest_position}
 
 def get_route_info():
 
     api_url = '{0}'.format(api_url_base)
 
-    response = requests.get(api_url, params=url_arguments)
+    response = requests.get(api_url, params=url_arguments_with_place_latlong)
     #print(response.url)
     #print(response.status_code) - success code - 200, failure code - others (400 - for wrong formatted url)
 
@@ -26,4 +29,37 @@ def get_route_info():
         return None
 
 route_info_json = get_route_info()
-print(route_info_json['routes'])
+#print(route_info_json['routes'])
+
+print("Places Array")
+print(route_info_json['places'])
+print("-----------------------")
+print("Airlines")
+print(route_info_json['airlines'])
+print("-----------------------")
+print("Aircrafts")
+print(route_info_json['aircrafts'])
+print("-----------------------")
+print('Agencies Array')
+print(route_info_json['agencies'])
+print('------------------------')
+print('Vehicles Array')
+print(route_info_json['vehicles'])
+print("------------------------")
+
+
+print("Routes Data",'\n')
+i = 0
+for route_dict in route_info_json['routes']:
+    i = i+1
+    print("Route Number", i)
+    for key,value in route_dict.items():
+        if key!= 'segments':
+            print(key,"-----",value)
+        else:
+            print(key,'--------')
+            for segment in value:
+                print('\t\t',segment)
+                print('\t\t','-------')
+    print("-----------------------------")
+    print("-----------------------------")
