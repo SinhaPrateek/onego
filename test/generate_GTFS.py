708381248ba663617031c3f476c286310b35c407
Prateek Sinha <prateeksinha.kgp@gmail.com>
2020-11-10 18:49:35 +0530

    changes to GTFS_generator

--- test/generate_GTFS.py
+++ test/generate_GTFS.py
@@ -1,7 +1,7 @@
 import pandas as pd
+import re
 
 file_indigo = r"/home/xena/Desktop/prateek/oneGo/onego_poc1/files/flight_schedules/FlightSchedule_Indigo.xlsx"
-
 schedule_indigo = pd.read_excel(file_indigo)
 
 
